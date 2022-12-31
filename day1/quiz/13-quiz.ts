c0f85f78215a2b4bf8c2657442a425b3608f73c2
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2022-12-31 08:12:11 +0700

    Update lesson day 1

---
+++ day1/quiz/13-quiz.ts
@@ -0,0 +1,33 @@
+function isPalindrome(angka: any): string {
+    try {
+        
+        // Cek apakah input merupakan number dengan menggunakan typeof
+        if (isNaN(angka)) {
+          throw new Error(`FAILED  : ${angka} is not a number`);
+        }
+      
+        // Cek apakah input merupakan 4 digit angka dengan menggunakan .toString()
+        // dan .length
+        if (angka.toString().length !== 4) {
+          throw new Error(`FAILED  : ${angka} is not a 4-digit number`);
+        }
+      
+        // Cek apakah input merupakan palindrome dengan membandingkan digit pertama
+        // dan terakhir, kedua dan sebelum terakhir, dan seterusnya
+        if (angka.toString().charAt(0) === angka.toString().charAt(3) &&
+            angka.toString().charAt(1) === angka.toString().charAt(2)) {
+          return `SUCCESS : ${angka} is palindrome`;
+        } else {
+          throw new Error(`FAILED  : ${angka} is not palindrome`);
+        }
+
+    } catch (error : any) {
+        return error.message
+    }
+}
+
+console.info(isPalindrome("abcd")); // "abcd is not a number"
+console.info(isPalindrome("123a")); // "123 is not a number"
+console.info(isPalindrome("1234")); // "1234 is not palindrome"
+console.info(isPalindrome(2112)); // "1221 is palindrome"
+console.info(isPalindrome("8888")); // "8888 is palindrome"
